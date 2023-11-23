370e9271c7de5cafe7a58ee85b57cecba7641b83
michaljones <micheal.jones2010@yahoo.com>
2023-11-22 21:57:19 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // code your solution here
+
+
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+
+  ]
+
+
+function superbowlWin(record){
+  const won = record.find(iswin);
+  console.log(won)
+if (won===undefined){
+
+    return undefined;
+
+  }
+  return won["year"]
+}
+
+
+
+
+
+
+
+function iswin(object){
+  return object.result === "W";
+  
+
+
+}
+
